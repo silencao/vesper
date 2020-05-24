2abc3be4b7b2935eb68a6a7cfd7c689f9a5389ca
Jinyan <imcaojy@outlook.com>
2020-05-24 14:13:37 +0800

    添加登录界面的组件

--- src/test/java/im/silen/vueboot/VueBootApplicationTests.java
+++ src/test/java/im/silen/vueboot/VueBootApplicationTests.java
@@ -6,11 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import im.silen.vueboot.growth.Growth;
 import im.silen.vueboot.util.JSONArray;
+import im.silen.vueboot.util.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.BufferedReader;
@@ -26,26 +29,16 @@ import java.util.stream.Collectors;
 @SpringBootTest
 class VueBootApplicationTests {
     @Autowired
-    private StringRedisTemplate redisTemplate;
+    private RedisTemplate<Object, Object> redisTemplate;
     @Autowired
     private ObjectMapper mapper;
     @Value("classpath:growths.json")
     private Resource resource;
 
     @Test
-    void mapperReadTest() throws IOException {
-        try (InputStream jsonStream = resource.getInputStream();
-             InputStreamReader in = new InputStreamReader(jsonStream);
-             BufferedReader bufferedReader = new BufferedReader(in)
-        ) {
-            String json = bufferedReader.lines().collect(Collectors.joining());
-            System.out.println(JSONArray.parse(json, Growth.class));
-        }
-    }
-
-    @Test
     void contextLoads() throws IOException {
         TypeFactory typeFactory = mapper.getTypeFactory();
+        BoundListOperations<Object, Object> listOps = redisTemplate.boundListOps("growths:test");
 
         mapper.<List<HashMap<String, String>>>readValue(resource.getFile(), typeFactory.constructCollectionType(List.class, typeFactory.constructMapType(HashMap.class, String.class, String.class))).stream()
                 .sorted(Comparator.comparing(o -> ZonedDateTime.parse(o.get("date"))))
@@ -57,11 +50,10 @@ class VueBootApplicationTests {
                 }, Map::putAll)
                 .forEach((localDate, growth) -> {
                     try {
-                        redisTemplate.opsForList().rightPush("growths:test", mapper.writeValueAsString(growth));
+                        listOps.leftPush(JSONObject.stringify(growth));
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
                     }
                 });
-
     }
 }
--- src/test/java/im/silen/vueboot/growth/GrowthServiceTest.java
+++ src/test/java/im/silen/vueboot/growth/GrowthServiceTest.java
@@ -2,12 +2,20 @@ package im.silen.vueboot.growth;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import im.silen.vueboot.util.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,19 +23,17 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class GrowthServiceTest {
     @Autowired
-    StringRedisTemplate redisTemplate;
+    RedisTemplate<Object, Object> redisTemplate;
     @Autowired
     ObjectMapper objectMapper;
+    @Value("classpath:growths.json")
+    private Resource resource;
+    @Test
+    void add() throws IOException {
+    }
 
     @Test
     void search() {
-        redisTemplate.opsForList().range("growths:test", -1, 0).stream().map(s -> {
-            try {
-                return objectMapper.readValue(s, Growth.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-                return new ArrayList<>();
-            }
-        }).forEach(System.out::println);
+        System.out.println(redisTemplate.boundListOps("growths:test").range(0, -1));
     }
 }
