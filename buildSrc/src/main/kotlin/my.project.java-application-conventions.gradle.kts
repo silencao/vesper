af72befcbd29090b0f6e5d6c4616f48287619656
silencao <imcaojy@foxmail.com>
2021-01-16 12:05:28 +0800

    fix build

--- buildSrc/src/main/kotlin/my.project.java-application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/my.project.java-application-conventions.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation(project(":vesper-spring-boot-starter"))
 //    implementation("org.springframework.boot:spring-boot-starter-webflux")
 //    testImplementation("io.projectreactor:reactor-test")
-//    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> {
---
+++ vesper-spring-boot-application/rest-client/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("my.project.java-application-conventions")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-json")
+}
