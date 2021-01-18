4cb08b2d4b188dbd3102bcf7e51a83c92e6bbab2
silencao <imcaojy@foxmail.com>
2021-01-18 23:06:59 +0800

    Change gradle-eclipse-plugin build path.

--- buildSrc/src/main/kotlin/my.project.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/my.project.java-common-conventions.gradle.kts
@@ -1,5 +1,10 @@
+import org.gradle.api.Action
+import org.gradle.plugins.ide.eclipse.model.Classpath
+import org.gradle.plugins.ide.eclipse.model.SourceFolder
+
 plugins {
     java
+    eclipse
     id("io.spring.dependency-management")
 }
 
@@ -10,6 +15,31 @@ dependencyManagement {
     }
 }
 
+/**
+ * 修改 eclipse build path
+ * 和 gradle build 到一起
+ * 默认是生成bin目录
+ */
+eclipse.classpath {
+    defaultOutputDir = buildDir
+
+    val dir = buildDir.toRelativeString(buildDir.parentFile)
+    val action = Action<Classpath> {
+        entries.filter {
+            it.kind == "src"
+        }.forEach {
+            it as SourceFolder
+            it.output = when (it.path) {
+                "src/main/java" -> "$dir/classes/java/main"
+                "src/test/java" -> "$dir/classes/java/test"
+                "src/main/resources" -> "$dir/resources/main"
+                "src/test/resources" -> "$dir/resources/test"
+                else -> ""
+            }
+        }
+    }
+    file.whenMerged(action)
+}
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
