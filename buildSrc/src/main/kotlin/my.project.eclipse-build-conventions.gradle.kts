9a111069411d75cebd9e78be317aa2e52228a346
silencao <imcaojy@foxmail.com>
2021-01-19 23:57:49 +0800

    buildSrc: eclipse编译路径调整

---
+++ buildSrc/src/main/kotlin/my.project.eclipse-build-conventions.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    eclipse
+}
+/**
+ * 修改 eclipse build path
+ * 和 gradle build 到一起
+ * 默认是生成bin目录
+ */
+eclipse.classpath {
+    val dir = buildDir.toRelativeString(buildDir.parentFile)
+    val action = Action<org.gradle.plugins.ide.eclipse.model.Classpath> {
+        entries.filterIsInstance<org.gradle.plugins.ide.eclipse.model.SourceFolder>().forEach {
+            it.output = when (it.path) {
+                "src/main/java"      -> "$dir/classes/java/main"
+                "src/test/java"      -> "$dir/classes/java/test"
+                "src/main/resources" -> "$dir/resources/main"
+                "src/test/resources" -> "$dir/resources/test"
+                else -> throw UnsupportedOperationException("无效的路径：${it.path}")
+            }
+        }
+    }
+    file.whenMerged(action)
+}
