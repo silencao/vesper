df66eb2f8b979d0b5dc03739d2917a58b54c3e6b
silencao <imcaojy@foxmail.com>
2021-02-24 23:05:10 +0800

    更新版本

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.2")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.4.3")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
     /**
      * kotlin项目构建必须依赖此插件
