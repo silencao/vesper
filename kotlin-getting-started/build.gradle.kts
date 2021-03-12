e4ddc4de733582917d8ccaacd151eb110214bfc5
silencao <imcaojy@foxmail.com>
2021-03-12 21:54:11 +0800

    kotlin build fat JAR

--- kotlin-getting-started/build.gradle.kts
+++ kotlin-getting-started/build.gradle.kts
@@ -11,8 +11,20 @@ application {
     mainClass.set("demo.MainKt")
 }
 
-/*tasks.jar {
-    manifest.attributes(
-        "Main-Class" to application.mainClass.get()
-    )
-}*/
+/* Creating a Java uber or fat JAR
+ --> https://docs.gradle.org/current/userguide/building_java_projects.html#sec:java_packaging */
+tasks.jar {
+    archiveClassifier.set("uber")
+
+    manifest.attributes("Main-Class" to application.mainClass.get())
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        sourceSets.main.get().output
+        configurations.runtimeClasspath
+            .get()
+            .filter { it.name.endsWith("jar") }
+            .map    { zipTree(it) }
+    })
+
+}
