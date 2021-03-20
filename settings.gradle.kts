21b50ab86c13ba63a34d12fe2a8dd21f9723fe5b
silencao <imcaojy@foxmail.com>
2021-03-20 08:53:26 +0800

    继续画饼，新建gradle-plugin模块

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,6 @@ include("vesper-spring-boot-starter")
 // 测试模块
 include("vesper-spring-boot-test")
 // 其他模块
+include("vesper-gradle-plugin")
 include("vesper-js-application")
 include("kotlin-getting-started")
---
+++ vesper-gradle-plugin/src/main/kotlin/js/nodejs/NodeJsPlugin.kt
@@ -0,0 +1,10 @@
+package js.nodejs
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class NodeJsPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+
+    }
+}
