352c18ef3e5763046ee63005f654540efe4fcbaf
Jinyan <imcaojy@outlook.com>
2021-04-18 15:11:22 +0800

    upgrade插件输出路径改变

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
+plugins {
+    id("my.project.spring-boot-upgrade-plugin")
+}
+
 allprojects {
     group = "im.silen"
     version = "0.0.5"
+}
+
+tasks.withType<spring.boot.UpgradeSpringBootTask> {
+    outputFile = projectDir.resolve("vesper-gradle-plugin/${Project.GRADLE_PROPERTIES}")
 }
--- vesper-gradle-plugin/src/main/kotlin/spring/boot/UpgradeSpringBootTask.kt
+++ vesper-gradle-plugin/src/main/kotlin/spring/boot/UpgradeSpringBootTask.kt
@@ -1,5 +1,6 @@
 package spring.boot
 
+import gradle.kotlin.dsl.accessors._e00424364118eb94b028be99e96b91cf.dependencyManagement
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.WriteProperties
 import org.gradle.api.tasks.options.Option
@@ -7,10 +8,24 @@ import org.gradle.api.tasks.options.Option
 open class UpgradeSpringBootTask : WriteProperties() {
     @get: Input
     @set: Option(option = "use-version", description = "升级版本号")
-    var version = "2.3.3.RELEASE"
+    var version: String = ""
 
     override fun writeProperties() {
+        group = "build setup"
+        lineSeparator = System.lineSeparator()
+
+        if (version == "")
+            throw RuntimeException("未定义版本号！")
+
+        project.dependencyManagement.imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:$version")
+        }
+
         property("use.spring.boot.version", version)
+        property("use.kotlin.version",
+            project.dependencyManagement.importedProperties
+                .get("kotlin.version") ?: KotlinVersion.CURRENT
+        )
 
         super.writeProperties()
     }
