eac088dd7493dda7662b52800b31336ffe5c0b96
Jinyan <imcaojy@outlook.com>
2021-04-18 14:43:34 +0800

    kotlin("plugin.serialization")调用方式改变

--- vesper-demo/kotlin-getting-started/src/main/kotlin/demo/Main.kt
+++ vesper-demo/kotlin-getting-started/src/main/kotlin/demo/Main.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.json.Json
 fun main() {
     println("What's your name?")
     val name= readLine()
-    println("Hello ${Json.encodeToString(PackageJson(mapOf("test" to "echo $name")))}!")
+    println("Hello ${Json.encodeToString(PackageJson.serializer(), demo.PackageJson(mapOf("test" to "echo $name")))}!")
 }
