26999644c0aceb3890f0b2ee17c5efa5234ca41c
Jinyan <imcaojy@outlook.com>
2021-04-18 18:40:04 +0800

    wrapperTest

--- vesper-gradle-plugin/src/test/kotlin/WrapperTest.kt
+++ vesper-gradle-plugin/src/test/kotlin/WrapperTest.kt
@@ -1,4 +1,22 @@
+import org.gradle.api.tasks.wrapper.Wrapper
+import org.gradle.util.DistributionLocator
+import org.gradle.util.GradleVersion
+import java.net.URI
+import java.util.*
 import kotlin.test.Test
+
+open class WrapperUpgradeTask : Wrapper() {
+    override fun getDistributionUrl(): String {
+        val locator = object : DistributionLocator() {
+            override fun getDistributionFor(version: GradleVersion, type: String): URI {
+
+                return super.getDistributionFor(version, type)
+            }
+        }
+
+        return locator.getDistributionFor(gradleVersion, distributionType.name.toLowerCase(Locale.ENGLISH)).toString()
+    }
+}
 
 class WrapperTest {
     @Test
