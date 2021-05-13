e76e51496a5935c3162bbea2183c54fda41c0503
CaoJinYan <imcaojy@foxmail.com>
2021-05-13 13:09:27 +0800

    解决sync projects时的警告

--- gradle-plugin/kotlin/js/build.gradle.kts
+++ gradle-plugin/kotlin/js/build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.5.0"))
+    /* kotlin-dsl插件会在compileOnly配置中引入对应版本的stdlib和reflect
+     * 和下面的gradle-plugin依赖的版本发生冲突，所以只在runtimeOnly中引入 */
+    runtimeOnly(kotlin("gradle-plugin", "1.5.0"))
 }
