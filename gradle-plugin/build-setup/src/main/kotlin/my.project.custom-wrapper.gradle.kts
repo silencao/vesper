249dc8f73d06da49606626d5d754417190125070
CaoJinYan <imcaojy@foxmail.com>
2021-12-24 10:15:07 +0800

    gradlew wrapper --gradle-version 7.3.3

--- gradle-plugin/build-setup/src/main/kotlin/my.project.custom-wrapper.gradle.kts
+++ gradle-plugin/build-setup/src/main/kotlin/my.project.custom-wrapper.gradle.kts
@@ -1,14 +1,16 @@
 import my.project.gradle.plugin.BuildSetup
 import my.project.gradle.plugin.task.WrapperTask
+import org.gradle.util.GradleVersion
 
 
 tasks.replace("wrapper", WrapperTask::class).apply {
     doFirst {
         distributionType = Wrapper.DistributionType.ALL
-        distributionPath = "wrapper/dists"/* 腾讯镜像是真滴慢
+        distributionPath = "wrapper/dists"
+        /* 先不配镜像地址了，还没有官网快
         distributionUrl  = "https://mirrors.cloud.tencent.com/gradle/" +
-                "gradle-$gradleVersion-${distributionType.name.toLowerCase()}.zip"*/
-
+                "gradle-$gradleVersion-${distributionType.name.toLowerCase()}.zip"
+        */
         if (gradleVersion == GradleVersion.current().version) {
             println("版本未变更！将复制文件到以下路径：")
             BuildSetup.showPaths(gradle.includedBuilds)
